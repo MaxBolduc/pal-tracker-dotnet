db7fb42c75bed7667eb7eb9edac319be28bce02d
Maxime Bolduc <bolduc.max@gmail.com>
2018-09-18 15:09:41 -0400

    Ajout de swagger

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Swashbuckle.AspNetCore.Swagger;
-using Swashbuckle.Swagger;
 
 namespace PalTracker
 {
